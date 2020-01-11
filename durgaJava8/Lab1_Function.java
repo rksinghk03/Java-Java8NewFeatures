3bacd0e4dbe25ee931c268b4e87834d7c6b916b4
RajSingh <RajSingh@9.81.219.67>
2020-01-11 10:39:41 +0530

    added

---
+++ durgaJava8/Lab1_Function.java
@@ -0,0 +1,14 @@
+package durgaJava8;
+
+import java.util.function.Function;
+
+public class Lab1_Function {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Function <Integer,Integer> f= i-> i*i;
+System.out.println("the squar fo 4 = "+f.apply(4));
+System.out.println("the squar of 5 = "+f.apply(5));
+	}
+
+}
---
+++ durgaJava8/Lab2_addNumber.java
@@ -0,0 +1,14 @@
+package durgaJava8;
+
+import java.util.function.Predicate;
+
+public class Lab2_addNumber {
+
+	public static void main(String[] args) {
+		Predicate <Integer> p=i->i%2==0;
+		
+System.out.println("Nummber = " +p.test(4));
+System.out.println("Nummber = " +p.test(5));
+	}
+
+}
